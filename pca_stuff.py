036148e019c8d89c7aeb70df26a5d79ae63148ba
zhangaaron <aaron.zhang@berkeley.edu>
2015-05-05 21:11:56 -0700

    shit is working

--- pca_stuff.py
+++ pca_stuff.py
@@ -35,34 +35,44 @@ def do_predictions(ratings):
         percentage_correct = np.sum(answers) / float(num_lines)
         print 'percentage correct: {0}'.format(percentage_correct)
 
+
 def nan_mask(matrix):
     matrix = np.abs(matrix)
-    matrix = matrix + 1  # broadcast this
-    
+    matrix = matrix + 1.0  # broadcast this
+    matrix = np.nan_to_num(matrix)
+    return preprocessing.binarize(matrix)
+
+
 class PICA(object):
     user_matrix = None
     jokes_matrix = None
     ratings = None
-    LAM = 1.0
-    ETA = 0.01
+    LAM = 30
+    ETA = 0.00001
     nan_mask = None
+    ratings = None
 
     def __init__(self, ratings, d=2):
-        self.ratings = ratings
+        self.ratings = np.nan_to_num(ratings)
+        assert not np.isnan(self.ratings).any()
         self.user_matrix = np.random.rand(ratings.shape[0], d)
         self.jokes_matrix = np.random.rand(ratings.shape[1], d).T
         self.jokes_matrix = self.jokes_matrix.T
-        self.nan_mask = np.isnan(self.ratings)
+        self.nan_mask = nan_mask(ratings)
+        assert not np.isnan(self.nan_mask).any()
+        print self.nan_mask
 
     def do_users_update(self):
-        nan_MSE = np.multiply(np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings, self.nan_mask)
-        MSE_X_V = np.dot(nan_MSE, self.jokes_matrix)
+        MSE_X_V = np.dot(np.multiply(np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings, self.nan_mask), self.jokes_matrix)
+        #MSE_X_V = np.dot(np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings, self.jokes_matrix)
+        assert np.dot(self.user_matrix, self.jokes_matrix.T).shape == self.ratings.shape
         self.user_matrix += -2 * self.ETA * (MSE_X_V + self.LAM * self.user_matrix)
 
     def do_jokes_update(self):
-        nan_MSE = np.multiply(np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings, self.nan_mask)
-        # print nan_MSE
-        self.jokes_matrix += -2 * self.ETA * (2 * np.dot(self.user_matrix.T, nan_MSE) + 2 * self.LAM * self.jokes_matrix.T).T
+        # MSE = np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings
+        # assert not np.isnan(MSE).any()
+        MSE = np.multiply(np.dot(self.user_matrix, self.jokes_matrix.T) - self.ratings, self.nan_mask)
+        self.jokes_matrix += -2 * self.ETA * (2 * np.dot(self.user_matrix.T, MSE) + 2 * self.LAM * self.jokes_matrix.T).T
 
     def do_predictions(self):
         MSE = np.sum((np.dot(self.user_matrix, self.jokes_matrix.T) - ratings) ** 2)
@@ -77,13 +87,16 @@ class PICA(object):
             percentage_correct = np.sum(answers) / float(num_lines)
             print 'percentage correct: {0}'.format(percentage_correct)
 
+    def write_test_set(self):
+        
+
     def gradient_descent(self, iterations=10000, debug=True):
         for i in xrange(iterations):
-            if i % 1000 == 0 and debug:
+            if i % 500 == 0 and debug:
+                print 'iteration # {0}'.format(i)
                 self.do_predictions()
             self.do_users_update()
             self.do_jokes_update()
-
 
 data = io.loadmat("./joke_data/joke_train.mat")
 ratings = np.nan_to_num(data['train'])
